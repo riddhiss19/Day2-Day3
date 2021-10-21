d65e4b874f19d5d6d697c9d285a5e9251c60e43d
riddhiss19 <78254144+riddhiss19@users.noreply.github.com>
2021-10-21 14:51:21 +0530

    Assignment for day 2 and day 3

---
+++ EmployeeDetails.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+class Employee 
+{
+	private int eid;
+	private String name=new String(" ");
+	private float salary;
+
+	public void inputDetails()
+	{
+		Scanner s=new Scanner(System.in);
+		System.out.print("Enter id: ");
+		eid=s.nextInt();
+
+		System.out.println("Enter name: ");
+		name=s.next();
+
+		System.out.print("Enter Salary: ");
+		salary=s.nextFloat();
+	}
+	public void showDetails()
+	{
+		System.out.print("Employee details:\n");
+		System.out.println("ID:"+eid+"\nName:"+name+"\nSalary:"+salary);
+	}
+}
+class EmployeeDetails 
+{
+	public static void main(String[] args) {
+		Employee e=new Employee();
+		e.inputDetails();
+		e.showDetails();
+	}
+}
